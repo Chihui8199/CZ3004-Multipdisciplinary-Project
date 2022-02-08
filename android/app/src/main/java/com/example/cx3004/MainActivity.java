c03e8008d839b71e8fe85ff76a6d6b799294836c
Chi Hui <chihui8199@gmail.com>
2022-02-09 02:36:14 +0800

    Latest update for all bluetooth fixes

--- android/app/src/main/java/com/example/cx3004/MainActivity.java
+++ android/app/src/main/java/com/example/cx3004/MainActivity.java
@@ -1,77 +1,256 @@
 package com.example.cx3004;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-import androidx.appcompat.widget.PopupMenu;
-import androidx.fragment.app.Fragment;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    // views
-    TextView pageTitle;
+    // Declaration Variables
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+    private static Context context;
 
-    // fragments
-    BluetoothSettingsFragment bluetoothSettingsFragment;
-    ImageRecognitionFragment imageRecognitionFragment;
-    FastestCarTaskFragment fastestCarTaskFragment;
+    BluetoothConnectionService mBluetoothConnection;
+    BluetoothDevice mBTDevice;
+    private static UUID myUUID;
+    ProgressDialog myDialog;
+
+    private static final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Initialization
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        viewPager.setOffscreenPageLimit(9999);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, new IntentFilter("incomingMessage"));
 
-        // instantiate views
-        pageTitle = (TextView) findViewById(R.id.page_title);
+        // Set up sharedPreferences
+        MainActivity.context = getApplicationContext();
+        this.sharedPreferences();
+        editor.putString("message", "");
+        //editor.putString("direction", "None");
+        editor.putString("connStatus", "Disconnected");
+        editor.commit();
 
-        // instantiate fragments
-        bluetoothSettingsFragment = new BluetoothSettingsFragment();
-        imageRecognitionFragment = new ImageRecognitionFragment();
-        fastestCarTaskFragment = new FastestCarTaskFragment();
 
-        // set page fragment
-        setPage(imageRecognitionFragment, R.string.title_image_recognition);
+        // Toolbar
+        Button bluetoothButton = (Button) findViewById(R.id.bluetoothButton);
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent popup = new Intent(MainActivity.this, BluetoothPairingPage.class);
+                startActivity(popup);
+            }
+        });
 
+        myDialog = new ProgressDialog(MainActivity.this);
+        myDialog.setMessage("Waiting for other device to reconnect...");
+        myDialog.setCancelable(false);
+        myDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
     }
 
-    public void setPage(Fragment fragment, int title){
-        pageTitle.setText(title);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.page_fragment, fragment)
-                .commit();
+    public static void sharedPreferences() {
+        sharedPreferences = MainActivity.getSharedPreferences(MainActivity.context);
+        editor = sharedPreferences.edit();
     }
 
-    public void showPopup(View v) {
-        PopupMenu popup = new PopupMenu(this, v);
-        popup.setOnMenuItemClickListener(
-                new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.bluetooth_settings_option:
-                                setPage(bluetoothSettingsFragment, R.string.title_bluetooth_settings);
-                                return true;
-                            case R.id.image_recognition_option:
-                                setPage(imageRecognitionFragment, R.string.title_image_recognition);
-                                return true;
-                            case R.id.fastest_car_task_option:
-                                setPage(fastestCarTaskFragment, R.string.title_fastest_car_task);
-                                return true;
-                            default:
-                                return false;
-                        }
-                    }
+    // Send message to bluetooth
+    public static void printMessage(String message) {
+        showLog("Entering printMessage");
+        editor = sharedPreferences.edit();
+
+        if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+            byte[] bytes = message.getBytes(Charset.defaultCharset());
+            BluetoothConnectionService.write(bytes);
+        }
+        showLog(message);
+        editor.putString("message", CommsFragment.getMessageReceivedTextView().getText() + "\n" + message);
+        editor.commit();
+        refreshMessageReceived();
+        showLog("Exiting printMessage");
+    }
+
+    public static void printMessage(String name, int x, int y) throws JSONException {
+        showLog("Entering printMessage");
+        sharedPreferences();
+
+        JSONObject jsonObject = new JSONObject();
+        String message;
+
+        switch (name) {
+//            case "starting":
+            case "waypoint":
+                jsonObject.put(name, name);
+                jsonObject.put("x", x);
+                jsonObject.put("y", y);
+                message = name + " (" + x + "," + y + ")";
+                break;
+            default:
+                message = "Unexpected default for printMessage: " + name;
+                break;
+        }
+        editor.putString("message", CommsFragment.getMessageReceivedTextView().getText() + "\n" + message);
+        editor.commit();
+        if (BluetoothConnectionService.BluetoothConnectionStatus == true) {
+            byte[] bytes = message.getBytes(Charset.defaultCharset());
+            BluetoothConnectionService.write(bytes);
+        }
+        showLog("Exiting printMessage");
+    }
+
+    public static void refreshMessageReceived() {
+        CommsFragment.getMessageReceivedTextView().setText(sharedPreferences.getString("message", ""));
+    }
+
+    public static void receiveMessage(String message) {
+        showLog("Entering receiveMessage");
+        sharedPreferences();
+        editor.putString("message", sharedPreferences.getString("message", "") + "\n" + message);
+        editor.commit();
+        showLog("Exiting receiveMessage");
+    }
+
+    private static void showLog(String message) {
+        Log.d(TAG, message);
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return context.getSharedPreferences("Shared Preferences", Context.MODE_PRIVATE);
+    }
+
+    private BroadcastReceiver mBroadcastReceiver5 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            BluetoothDevice mDevice = intent.getParcelableExtra("Device");
+            String status = intent.getStringExtra("Status");
+            sharedPreferences();
+
+            if (status.equals("connected")) {
+                try {
+                    myDialog.dismiss();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
                 }
-        );
-        MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.nav_menu, popup.getMenu());
-        popup.show();
+
+                Log.d(TAG, "mBroadcastReceiver5: Device now connected to " + mDevice.getName());
+                Toast.makeText(MainActivity.this, "Device now connected to " + mDevice.getName(), Toast.LENGTH_LONG).show();
+                editor.putString("connStatus", "Connected to " + mDevice.getName());
+
+            } else if (status.equals("disconnected")) {
+                Log.d(TAG, "mBroadcastReceiver5: Disconnected from " + mDevice.getName());
+                Toast.makeText(MainActivity.this, "Disconnected from " + mDevice.getName(), Toast.LENGTH_LONG).show();
+
+
+                editor.putString("connStatus", "Disconnected");
+
+
+                myDialog.show();
+            }
+            editor.commit();
+        }
+    };
+
+    BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("receivedMessage");
+            showLog("receivedMessage: message --- " + message);
+            // write specific commands here
+            sharedPreferences();
+            String receivedText = sharedPreferences.getString("message", "") + "\n" + message;
+            editor.putString("message", receivedText);
+            editor.commit();
+            refreshMessageReceived();
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 1:
+                if (resultCode == Activity.RESULT_OK) {
+                    mBTDevice = (BluetoothDevice) data.getExtras().getParcelable("mBTDevice");
+                    myUUID = (UUID) data.getSerializableExtra("myUUID");
+                }
+        }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(messageReceiver);
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver5);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver5);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            IntentFilter filter2 = new IntentFilter("ConnectionStatus");
+            LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver5, filter2);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        showLog("Entering onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+
+        outState.putString(TAG, "onSaveInstanceState");
+        showLog("Exiting onSaveInstanceState");
+    }
 }
