ccc71d174af56240d46cf44c9597c600395a4a44
Y Xiao <xiao0100@e.ntu.edu.sg>
2022-03-03 11:17:35 +0800

    update on server

--- algorithm/image_rec/img_rec.py
+++ algorithm/image_rec/img_rec.py
@@ -169,7 +169,6 @@ class sync:
     def __init__(self):
         self.exit_flag = False
         self.detect_sem = threading.Lock()
-        self.detection = []
 
     def stop_async(self, thread):
         self.exit_flag = True
@@ -180,7 +179,6 @@ class sync:
             time.sleep(3)  # take photo every x + 2 seconds
             self.detect_sem.acquire()
             id, id_num, dist, angle = detect()
-            self.detection.append(detect())
             print("ASYNC TAKE PHOTO")
             self.detect_sem.release()
             if self.exit_flag:
